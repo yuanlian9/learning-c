f78390005a909392dfbbeccb866d19fae3ddd503
圆脸 <1664815002@>
2020-08-07 08:53:55 +0800

    判定字符串是否相等

---
+++ tcwyj.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+	char* str1 = "haha";
+	char* str2 = "hehe";
+	if (str1 == str2) {
+		printf("相等\n");
+
+	}
+	else{
+		printf("不相等\n");
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
