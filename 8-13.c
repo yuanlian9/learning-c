3b93fd56a847ff233a62fb5a1da832092788b7e2
圆脸 <1664815002@>
2020-08-19 15:27:58 +0800

    练习

---
+++ 8-13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+int sum(int a)
+{
+	int c = 0;
+	static int b = 3;
+	c += 1;
+	b += 2;
+	return (a + b + c);
+}
+int main()
+{
+	int i;
+	int a = 2;
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d,", sum(a));
+	}
+	system("pause");
+}
